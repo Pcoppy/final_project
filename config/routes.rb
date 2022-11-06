fdb2bd3b38de66298dd180570d63ac86bd14ede5
Pierre Coppermann <pcoppy@gmail.com>
2022-11-06 19:40:47 +0100

    start to producer routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :producer, only: [:new, :create]
+  end
 
+  resources :producers, only: [:edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
