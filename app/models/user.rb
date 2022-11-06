32dbb57f85b9b8f21b2feb27c3e91902ac646d5f
Pierre Coppermann <pcoppy@gmail.com>
2022-11-06 18:31:17 +0100

    started the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :editor
+  has_many :books, through: :editors
+  has_one :producer
+  has_many :views, through: :producer, dependent: :destroy
+  has_one_attached :photo
 end
