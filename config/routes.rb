62de6f8169a944c12ecb3bcc5008c0a11d839e16
Pierre Coppermann <pcoppy@gmail.com>
2022-11-08 12:03:42 +0100

    added routes and initialized controller for editors

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show] do
     resources :producer, only: [:new, :create]
+    resources :editors, only: [:new, :create]
   end
 
-  resources :producers, only: [:edit, :update, :destroy]
+  resources :producers, only: [:edit, :show, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :editors, only: [:edit, :show, :index, :update, :destroy]
 end
