4a152de1a011442d33812bc0947c8db6b54f977c
Pierre Coppermann <pcoppy@gmail.com>
2022-11-08 16:38:27 +0100

    controller-editor initialized

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -1,2 +1,51 @@
 class EditorsController < ApplicationController
+  def index
+    @editors = policy_scope(Editor)
+  end
+
+  def show
+    @editor = Editor.find(params[:id])
+    authorize @editor
+  end
+
+  def new
+    @editor = Editor.new
+    authorize @editor
+  end
+
+  def create
+    @editor = Editor.new(editors_params)
+    @editor.user = current_user
+    authorize @editor
+    if @editor.save
+      redirect_to editor_path(@editor)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @editor = Editor.find(params[:id])
+    authorize @editor
+  end
+
+  def update
+    @editor = Editor.find(params[:id])
+    @editor.update(editors_params)
+    authorize @editor
+    redirect_to editor_path(@editor)
+  end
+
+  def destroy
+    @editor = Editor.find(params[:id])
+    authorize @editor
+    @editor.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def editors_params
+    params.require(:editor).permit(:name, :country, :address)
+  end
 end
