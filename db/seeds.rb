a0ed0b51ce896b1bc839d993fda0360dbf69e543
Pierre Coppermann <pcoppy@gmail.com>
2022-11-09 14:56:05 +0100

    navbar-redesign and seed-fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p "Producers reset"
+Producer.destroy_all
+p "Editors reset"
+Editor.destroy_all
+p "Users reset"
+User.destroy_all
 
-
-pierre = User.create(email: "pcoppy@gmail.com", password: "123456", subscription: "none")
+pierre = User.create(email: "pcoppy@gmail.com", password: "123456", first_name: "Pierre", last_name: "Coppermann", username:"pcoppy", subscription: "none")
+p "Users created"
 
 pierre_producer = Producer.create(user_id: pierre.id, name: "Pierre Coppermann", company: "CoppermannProd", interests: "action,romance")
+p "Producers created"
+
 pierre_editor = Editor.create(user_id: pierre.id, name: "Pierre Coppermann", address: "Paris", country: "France")
+p "Editors created"
