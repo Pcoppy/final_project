ecb203508d4485b4f4e3a9a8e6a704af5975cf17
Pierre Coppermann <pcoppy@gmail.com>
2022-11-14 23:02:20 +0100

    added producer_policy

---
+++ app/policies/producer_policy.rb
@@ -0,0 +1,32 @@
+class ProducerPolicy < ApplicationPolicy
+  def show?
+    record.user == user || (record.wishlist & user.books) != []
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def new?
+    user.subscription != "none" && !user.producer
+  end
+
+  def create?
+    user.subscription != "none" && !user.producer
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
