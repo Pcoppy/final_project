a3a280e353481f951b96546d5ab722b0ab98c0cd
Pierre Coppermann <pcoppy@gmail.com>
2022-11-15 17:38:48 +0100

    js works on active for the pictures

--- app/javascript/controllers/homepage_controller.js
+++ app/javascript/controllers/homepage_controller.js
@@ -14,14 +14,30 @@ function displayTutorial () {
   const tutorialContainer = document.querySelector(".tutorial-container")
   tutorialContainer.classList.add('active')
 }
+function displayTest(navItem) {
+  console.log(navItem)
+}
+function togglePic (element) {
+  const pictures = document.querySelectorAll(".nav-link-photo")
+  console.log(pictures)
+  console.log(element.target.parentElement.children[0].classList)
+  console.log(pictures[0])
+  pictures.forEach(picture => {
+    picture.classList.remove('active')
+  })
+  element.target.parentElement.children[0].classList.add('active')
+}
 
 function helloTest () {
   console.log('Hello')
 }
+
+const parentNavItems = document.querySelectorAll(".scroll-on-click")
 const navItems = document.querySelectorAll(".scroll-on-click")
 navItems.forEach(navItem => {
   navItem.addEventListener("click", displayTutorial)
   navItem.addEventListener("click", scrollDown)
+  navItem.addEventListener("click", togglePic)
 })
 
 window.addEventListener("click", helloTest)
