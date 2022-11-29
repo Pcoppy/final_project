a0d08ada32c9dc9fae098f0738582ff1dbb2bde4
Pcoppy <pcoppy@gmail.com>
2022-11-29 12:40:53 +0100

    summarry and additional info fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_103936) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_113028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,8 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_103936) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "publishing_year"
-    t.string "summary"
-    t.string "additionnal_description"
+    t.text "summary"
+    t.text "additionnal_description"
     t.string "genre"
     t.bigint "editor_id", null: false
     t.bigint "author_id", null: false
